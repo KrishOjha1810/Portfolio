c43c93491c96a3e420114551e98e38ab49b449e0
Krish Ojha <11krishojha08@gmail.com>
2024-10-15 17:33:25 +0530

    Second commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -21,9 +21,9 @@ function Projects() {
               imgPath={WanderLust}
               isBlog={false}
               title="Tomato"
-              description="Tomato is a fully responsive food delivery website crafted with the MERN stack, featuring a visually stunning user interface and an exceptional user experience. Designed with seamless navigation in mind, the site allows users to browse menus, place orders, and manage their accounts effortlessly. The application incorporates user authentication and authorization for secure access and personalized interactions. Additionally, it integrates Stripe for a smooth payment processing experience, ensuring that transactions are quick and reliable. The project is built using the MVC architecture, promoting a clean and organized code structure that enhances maintainability and scalability.."
-              ghLink="https://github.com/KrishOjha1810/WanderLust"
-              demoLink="https://minor-project-0xl8.onrender.com/listings"
+              description="Tomato is a fully responsive food delivery website crafted with the MERN stack, featuring a visually stunning user interface and an exceptional user experience. Designed with seamless navigation in mind, the site allows users to browse menus, place orders, and manage their accounts effortlessly. An integrated admin panel connected to the backend provides seamless control over operations, allowing administrators to manage listings, track orders, and monitor user activity effectively. The application incorporates user authentication and authorization for secure access and personalized interactions. Additionally, it integrates Stripe for a smooth payment processing experience, ensuring that transactions are quick and reliable. The project is built using the MVC architecture, promoting a clean and organized code structure that enhances maintainability and scalability. Everything is deployed for easy access and management."
+              ghLink="https://github.com/KrishOjha1810/Food-Delivery"
+              demoLink="https://food-delivery-frontend-wnrp.onrender.com"
             />
           </Col>
           <Col md={4} className="project-card">
